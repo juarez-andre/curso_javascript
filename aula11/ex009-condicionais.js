e7f8fd0f3359ff9c26420f4d5a37cd64edb0c365
Juarez <juarez.andre2013@gmail.com>
2022-03-13 21:58:32 -0300

    novos exercicios

---
+++ aula11/ex009-condicionais.js
@@ -0,0 +1,11 @@
+// F8 para rodar o plugin Node Exec
+
+// console.log("O console funcionou corretamente.")
+
+var vel = 59
+
+console.log(`A velocidade do seu carro é ${vel}`)
+if (vel > 60) {
+    console.log("Você ultrapassou o limite de velocidade. MULTADO!")
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
