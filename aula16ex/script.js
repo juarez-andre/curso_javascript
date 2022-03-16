a0ac7b89590f78ba68c8d22ee013bc190c842e2d
Juarez <juarez.andre2013@gmail.com>
2022-03-16 20:27:08 -0300

    ultimo

--- aula16ex/script.js
+++ aula16ex/script.js
@@ -1,12 +1,38 @@
+var lista = []
 function guardar() {
     var num = document.getElementById("num")
     var tab = document.getElementById("tabela")
-    if (Number(num.value.length) == 0 || Number(num.value) > 100 || Number(num.value) < 0) {
-        alert('Por favor, digite um valor válido!')
-    } else {
-        var lista = []
-        lista.push(Number(num.value))
+    if (insights.length != 0){
+        insights.innerHTML = ''
     }
+    if (Number(num.value.length) == 0 || Number(num.value) > 100 || Number(num.value) < 0) {
+        window.alert('Por favor, digite um valor válido!')
+    } else {
+        lista.push(Number(num.value))
+        var item = document.createElement("option")
+        item.text += `O valor ${Number(num.value)} foi adicionado.`
+        tab.appendChild(item)
+    }
+}
+function mostrar() {
+    var num = document.getElementById("num")
+    var insights = document.getElementById("insights")
+    var maior = lista[0]
+    var menor = lista[0]
+    var soma = 0
+    for (var c in lista) {
+        if (lista[c] > maior) {
+            maior = lista[c]
+        }
+        if (lista[c] < menor) {
+            menor = lista[c]
+        }
+    }
+    for (var c in lista) {
+        soma += Number(lista[c])
+    }
+    var media = soma / lista.length
 
-
+    insights.innerHTML = `<p>Ao todo, temos ${lista.length} números cadastrados.</p><p>O maior valor informado foi ${maior}.</p><p>O menor valor informado foi ${menor}.</p><p>O valor da soma dos valores digitados é ${soma}.</p><p>A média dos valores digitados é ${media}.</p>`
+    
 }
